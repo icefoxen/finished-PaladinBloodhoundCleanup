13c59e1852cb12a70a8014f848507de0c4e99bc9
Simon Heath <icefoxen@gmail.com>
2014-02-04 10:32:39 -0600

    Initial import.
    This is intended to be a tool to handle the tedium of
    cleaning up and converting Bloodhound data into something
    that can be imported into Wellsight.  It will handle the
    following steps:
    
    1) Read in .xls file
    2) Remove unnecessary columns
    3) Clean out duplicate data
    4) Interpolate missing data
    5) Trim data to the desired depth range
    6) Export data to .las file (tab-delimited text)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace PaladinDataCleanup
+{
+	class MainClass : Form
+	{
+		public MainClass()
+		{
+			Text = "Simple";
+			Size = new Size(250, 200);
+			CenterToScreen();
+
+			//var t = new TextFieldParser();
+		}
+
+		static public void Main()
+		{
+			Application.Run(new MainClass());
+		}
+	}
+}
